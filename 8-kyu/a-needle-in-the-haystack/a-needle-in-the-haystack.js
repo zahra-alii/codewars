b593809452fe5e9b4c7097c8f076d0697f871a7f
Zahra Ali <zahraali.tech@gmail.com>
2025-03-05 16:04:55 -0500

    Add solution - CodeHub

---
+++ 8-kyu/a-needle-in-the-haystack/a-needle-in-the-haystack.js
@@ -0,0 +1,8 @@
+// SOLUTION 1
+​
+function findNeedle(haystack){
+return `found the needle at position ${haystack.indexOf("needle")}`
+}
+​
+// SOLUTION 1 EXPLAINED:
+// used temporal literals + .indexOf method to find the value "needle"
