96b0b35df3eac56599b5d8b6b7610b3e51c667a8
Zahra Ali <zahraali.tech@gmail.com>
2025-03-05 16:43:37 -0500

    Add solution - CodeHub

---
+++ 8-kyu/even-or-odd/even-or-odd.js
@@ -0,0 +1,5 @@
+const evenOrOdd = number => number % 2 === 0 ? "Even" : "Odd";
+​
+// SOLUTION EXPLAINED:
+// Use an arrow function with idenfier "evenOrOdd" that takes in the parameter "number"
+// Then, if the number is divisible by 2, return "Even". Else, return "Odd".
