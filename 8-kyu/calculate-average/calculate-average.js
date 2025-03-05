11c7ad13f4c62748a9a0c851df18a4d7724ce8f7
Zahra Ali <zahraali.tech@gmail.com>
2025-03-05 15:53:44 -0500

    Update calculate-average.js

--- 8-kyu/calculate-average/calculate-average.js
+++ 8-kyu/calculate-average/calculate-average.js
@@ -1,3 +1,23 @@
+// SOLUTION 1 - USING A FOR LOOP
+
+function findAverage(array) {
+if(array.length === 0){
+return 0;
+}
+let total = 0;
+// for loop that passes into param length
+for(let i=0; i<array.length; i++){
+total += array[i]/array.length;
+}
+return total;
+
+// SOLUTION 1 EXPLAINED:
+// create condition for empty arrays to return 0
+// declare total variable, set the value to 0
+// create for loop that passes through param length
+// add the total to every element passed into the array, then divide by total # of elements (array.length)
+  
+
 // SOLUTION 2
 ​
 function findAverage(array) {
