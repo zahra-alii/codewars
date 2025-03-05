ab83c92e15acb7c6f83dbb6bba87d4fed0f38e1e
Zahra Ali <zahraali.tech@gmail.com>
2025-03-05 17:10:00 -0500

    Update convert-number-to-reversed-array-of-digits.js

--- 8-kyu/convert-number-to-reversed-array-of-digits/convert-number-to-reversed-array-of-digits.js
+++ 8-kyu/convert-number-to-reversed-array-of-digits/convert-number-to-reversed-array-of-digits.js
@@ -1,3 +1,9 @@
 function digitize(n) {
   return String(n).split('').map(Number).reverse();
 }
+
+// SOLUTION EXPLAINED:
+// String(n) takes the numbers passed into it and makes them a string [Ex: String(123); // returns "123"]
+// .split('') splits the new string into an array of strings
+// .map() iterates/loops through each element in the array (in this case, Number)
+// .reverse() reverses the order of the array altogether
