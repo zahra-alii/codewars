63f038e8079d1373aeb4807705ce7bc4b30d7f3c
Zahra Ali <zahraali.tech@gmail.com>
2025-03-05 16:07:50 -0500

    Update a-needle-in-the-haystack.js

--- 8-kyu/a-needle-in-the-haystack/a-needle-in-the-haystack.js
+++ 8-kyu/a-needle-in-the-haystack/a-needle-in-the-haystack.js
@@ -1,4 +1,4 @@
-// SOLUTION 1
+// SOLUTION 1:
 ​
 function findNeedle(haystack){
 return `found the needle at position ${haystack.indexOf("needle")}`
@@ -6,3 +6,17 @@ return `found the needle at position ${haystack.indexOf("needle")}`
 ​
 // SOLUTION 1 EXPLAINED:
 // used temporal literals + .indexOf method to find the value "needle"
+
+
+// SOLUTION 2: For loop + Condition
+function findNeedle(arr) {
+for (let i=0; i<arr.length; i++){
+if (arr[i] === 'needle'){
+return 'found the needle at position ' + i;
+}
+}
+}
+
+// SOLUTION 2 EXPLAINED:
+// create a for loop going through the parameter length
+// set condition and return statement
