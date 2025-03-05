044f787d9a1048d16a7f7061d5a8d285a425dc50
Zahra Ali <zahraali.tech@gmail.com>
2025-03-05 16:49:55 -0500

    Add solution - CodeHub

---
+++ 8-kyu/remove-first-and-last-character-part-two/remove-first-and-last-character-part-two.js
@@ -0,0 +1,9 @@
+function array(string){
+  return string.split(",").slice(1,-1).join(" ") || null;
+}
+​
+// SOLUTION EXPLAINED:
+// Use the .split() method to split the comma from the output
+// Then, use .slice to remove the first and last characters in the string
+// Next, use the .join method to join the strings together with a space in between
+// Lastly, set the default output as NULL if string is empty / doesn't match conditions
