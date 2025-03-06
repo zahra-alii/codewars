3fa4c2dbdb85e5dbe528c1f9522f24338c486f66
Zahra Ali <zahraali.tech@gmail.com>
2025-03-06 15:46:29 -0500

    Update merge-two-sorted-arrays-into-one.js

--- 8-kyu/merge-two-sorted-arrays-into-one/merge-two-sorted-arrays-into-one.js
+++ 8-kyu/merge-two-sorted-arrays-into-one/merge-two-sorted-arrays-into-one.js
@@ -2,3 +2,9 @@
 function mergeArrays(arr1, arr2) {
   return Array.from(new Set(arr1.concat(arr2).sort((a,b) => a-b)));
 }
+
+// SOLUTION EXPLAINED:
+// Array.from() creates a shallow copy of the array
+// new Set() creates a new array
+// .concat() combines/concatenates both arrays together 
+// .sort() returns every element passed into the new array in order
