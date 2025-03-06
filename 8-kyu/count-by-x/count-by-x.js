b2dff79e1b740f2daae2c5f69227dfeed26df3d8
Zahra Ali <zahraali.tech@gmail.com>
2025-03-06 15:49:37 -0500

    Add solution - CodeHub

---
+++ 8-kyu/count-by-x/count-by-x.js
@@ -0,0 +1,13 @@
+function countBy(x, n) {
+  let z = [];
+  for(let i=1; i<=n; i++){
+   z.push(x*i); 
+  }
+    return z;
+}
+​
+// SOLUTION EXPLAINED:
+// create a new arrat (z)
+// create a for loop that passes through parameter n
+// push every element that is a multiple of x into the new arr
+// return the new array
