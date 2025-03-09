a4f4556bf9061ef594354fd413a97b31f6768e9d
Zahra Ali <zahraali.tech@gmail.com>
2025-03-09 11:20:15 -0400

    Add solution - CodeHub

---
+++ 8-kyu/find-multiples-of-a-number/find-multiples-of-a-number.js
@@ -0,0 +1,16 @@
+function findMultiples(int, limit){
+    let arr = [];
+    for(let i = int; i<=limit; i+=int){
+        arr.push(i)
+    }
+    return arr;
+  }
+​
+/* SOLUTION EXPLAINED:
+1 - create new empty array
+2 -set for loop that passes through param start & end
+3 - push every number that matches the condition into new array
+4 - return new array*/
+​
+​
+​
