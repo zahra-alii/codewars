a69c2c2fbfb74ceb341017dd8e499b3bbd4ed107
Zahra Ali <zahraali.tech@gmail.com>
2025-03-09 12:40:09 -0400

    Update counting-sheep-dot-dot-dot.js

--- 8-kyu/counting-sheep-dot-dot-dot/counting-sheep-dot-dot-dot.js
+++ 8-kyu/counting-sheep-dot-dot-dot/counting-sheep-dot-dot-dot.js
@@ -1,3 +1,15 @@
+// SOLUTION 1: .filter() method
 function countSheeps(sheep) {
   return sheep.filter(Boolean).length;
+}
+
+// SOLUTION 2: for loop
+function countSheeps(sheep){
+    let num = 0;
+    for(let i=0; i<sheep.length; i++){
+        if(sheep[i] === true){
+            num++;
+        }
+    }
+    return num;
 }
