7469760a3c72b22ed93fc85376f471d6499362b5
Zahra Ali <zahraali.tech@gmail.com>
2025-03-09 12:28:56 -0400

    Update beginner-reduce-but-grow.js

--- 8-kyu/beginner-reduce-but-grow/beginner-reduce-but-grow.js
+++ 8-kyu/beginner-reduce-but-grow/beginner-reduce-but-grow.js
@@ -1,2 +1,12 @@
+// SOLUTION 1: .reduce() method
 const grow = x => x.reduce((acc, c) => acc*c)
-​
+
+// SOLUTION 2: For Loop
+function grow(x){
+    // set total variable to 1
+    let prod = 1;
+    for(let i=0; i<x.length; i++){
+    prod *= x[i]
+}
+return prod;
+}​
