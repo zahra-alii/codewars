cb0d531583ab56838da11c945da626c16bec35ee
Zahra Ali <zahraali.tech@gmail.com>
2025-03-09 11:33:12 -0400

    Update solution - CodeHub

--- 8-kyu/reverse-list-order/reverse-list-order.js
+++ 8-kyu/reverse-list-order/reverse-list-order.js
@@ -1,3 +1,14 @@
+// SOLUTION 1:
+​
 function reverseList(list) {
-  return list.reverse();
+  let arr = []
+  for(let i=list.length - 1; i>=0; i--){
+    arr.push(list[i]);
+  }
+  return arr;
 }
+​
+/* SOLUTION EXPLAINED:
+- Create a new array to push elements on in line 4
+- Make for loop: Set starting point (initialization) equal to last element of the array (.length - 1)
+- return new array */
