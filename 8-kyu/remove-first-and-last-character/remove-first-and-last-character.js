2afaa885aa72a09d36465d6c759834fa9cacfdb0
Zahra Ali <zahraali.tech@gmail.com>
2025-03-10 09:52:01 -0400

    Update remove-first-and-last-character.js

--- 8-kyu/remove-first-and-last-character/remove-first-and-last-character.js
+++ 8-kyu/remove-first-and-last-character/remove-first-and-last-character.js
@@ -1,7 +1,14 @@
+// Solution 1:
 function removeChar(str){
-  return str.slice(1,-1);
+  return str.slice(1,-1); // -1 = same as "str.length - 1"
 };
 ​
-​
-​
+// Solution 2:
+ function removeChar(str){
+    str = str.split("")
+    str.shift(); // removes first char
+    str.pop(); // removes last char
+    str = str.join("") // joins new str together
+    return str;
+ }​
 ​
