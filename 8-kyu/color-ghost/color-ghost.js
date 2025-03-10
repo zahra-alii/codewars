f16da8240d100e48799dfdaf0a38379698a6ad35
Zahra Ali <zahraali.tech@gmail.com>
2025-03-10 13:15:21 -0400

    Update color-ghost.js

--- 8-kyu/color-ghost/color-ghost.js
+++ 8-kyu/color-ghost/color-ghost.js
@@ -3,3 +3,10 @@ class Ghost {
     this.color = ["white", "purple","yellow", "red"][Math.floor(Math.random()*4)] ;
   }
 }
+
+/* SOLUTION EXPLAINED:
+- when you have a question with instances in it, use "this" keyword on the property
+- put every value being passed in into an array
+- Math.random() returns random # between 0 to 1
+- Multiply by 4 since there are 4 values
+- Math.floor() returns largest number <= given number (estimates to the nearest whole #) /*
