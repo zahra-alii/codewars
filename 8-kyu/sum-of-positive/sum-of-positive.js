72fdfa987629621c2968b9a95971d3effba74d74
Zahra Ali <zahraali.tech@gmail.com>
2025-03-10 11:01:38 -0400

    Update sum-of-positive.js

--- 8-kyu/sum-of-positive/sum-of-positive.js
+++ 8-kyu/sum-of-positive/sum-of-positive.js
@@ -1,3 +1,4 @@
+// Solution 1: For loop
 function positiveSum(arr) {
   let total = 0;
   for(let i=0; i<=arr.length; i++){
@@ -14,3 +15,12 @@ function positiveSum(arr) {
 -set condition for all positive numbers
 - add each element to 0, return total. */
 ​
+// Solution 2: .reduce() + .filter() methods
+function positiveSum(arr) {
+  return arr.filter(x => x > 0).reduce((a, b) => a+b, 0);
+}
+
+/* SOLUTION EXPLAINED:
+- use .filter to filter out numbers that match condition (x > 0)
+- use iterate to iterate through every number, add them together and get back a new array
+- initial val = 0 */
