0db2e0a9e31e8cce927e8833723f4e53e85cc58d
Zahra Ali <zahraali.tech@gmail.com>
2025-03-11 19:30:36 -0400

    Add solution - CodeHub

---
+++ 8-kyu/classy-extentions/classy-extentions.js
@@ -0,0 +1,10 @@
+class Cat extends Animal {
+  speak () {
+    return `${this.name} meows.`
+  }
+}
+​
+/* SOLUTION EXPLAINED:
+- use "extends" keyword
+- use "this.name" keyword to return cat's name 
+- use temporal literals to combine strings  */
