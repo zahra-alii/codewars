6a9886af754ece70ea9ab25463886782cff7aff0
Zahra Ali <zahraali.tech@gmail.com>
2025-03-13 21:06:26 -0400

    Add solution - CodeHub

---
+++ 8-kyu/enumerable-magic-number-1-true-for-all/enumerable-magic-number-1-true-for-all.js
@@ -0,0 +1,6 @@
+function all( seq, func ){
+    return seq.every(func);
+}
+​
+// SOLUTION EXPLAINED:
+// -.every() tests every element passed in an array and return a Boolean value (true or false) 
