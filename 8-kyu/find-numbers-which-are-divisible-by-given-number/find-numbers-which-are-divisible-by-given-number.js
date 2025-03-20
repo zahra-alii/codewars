7d7fa8f97cd841ba341bcc0ca35c0a592f45f893
Zahra Ali <zahraali.tech@gmail.com>
2025-03-20 16:03:49 -0400

    Update find-numbers-which-are-divisible-by-given-number.js

--- 8-kyu/find-numbers-which-are-divisible-by-given-number/find-numbers-which-are-divisible-by-given-number.js
+++ 8-kyu/find-numbers-which-are-divisible-by-given-number/find-numbers-which-are-divisible-by-given-number.js
@@ -1,3 +1,4 @@
+// SOLUTION 1:
 function divisibleBy(numbers, divisor){
     return numbers.filter(x => x % divisor === 0);
 }
@@ -5,3 +6,14 @@ function divisibleBy(numbers, divisor){
 /* SOLUTION EXPLAINED:
 - use .filter() to filter out the elements that DON'T meet condition 
 and RETURN every element that is divisble by the divisor (in this case, 2) */
+
+// SOLUTION 2:
+ function divisibleBy(numbers, divisor){
+    let newArr = [];
+    for(let i = 0; i<numbers.length; i++){
+      if(numbers[i] % divisor === 0){
+      newArr.push(numbers[i]);
+    }
+  }
+  return newArr;
+}
