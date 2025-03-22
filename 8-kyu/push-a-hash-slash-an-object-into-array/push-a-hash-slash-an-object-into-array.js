ae34c94414c834813e721872b39131c5c9f2bb56
Zahra Ali <zahraali.tech@gmail.com>
2025-03-22 02:15:17 -0400

    Add solution - CodeHub

---
+++ 8-kyu/push-a-hash-slash-an-object-into-array/push-a-hash-slash-an-object-into-array.js
@@ -0,0 +1,13 @@
+const items = []
+​
+const itemsObject = {
+  a: "b",
+  c: "d"
+};
+​
+items.push(itemsObject);
+​
+/* STEPS:
+- declare new empty arr (items)
+- create object & declare vals
+- push the objects into the items arr */
