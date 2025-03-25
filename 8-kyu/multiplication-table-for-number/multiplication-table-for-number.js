872962492c788066933a06a939dc65f16006d6f8
Zahra Ali <zahraali.tech@gmail.com>
2025-03-25 17:13:22 -0400

    Update multiplication-table-for-number.js

--- 8-kyu/multiplication-table-for-number/multiplication-table-for-number.js
+++ 8-kyu/multiplication-table-for-number/multiplication-table-for-number.js
@@ -1,4 +1,4 @@
-// Solution:
+// SOLUTION 1:
 function multiTable(number) {
   return [...Array(10)].map((elem, index) => `${index + 1} * ${number} = ${(index + 1) * number}`).join('\n')
 }
@@ -6,3 +6,13 @@ function multiTable(number) {
 /* Steps:
 - Array() stores the numbers under single array
 - .map() iterates over every elem & index */
+
+// SOLUTION 2:
+function multiTable (number){
+  let table = ""; // decl empty str
+
+  for(let i=1; i<=10; i++){
+    table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
+  }
+  return table;
+}
