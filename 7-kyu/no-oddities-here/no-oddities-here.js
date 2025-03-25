e9a0d2771f0f4b75a54ff2baaf6b1c61306f18b5
Zahra Ali <zahraali.tech@gmail.com>
2025-03-25 16:48:24 -0400

    Update no-oddities-here.js

--- 7-kyu/no-oddities-here/no-oddities-here.js
+++ 7-kyu/no-oddities-here/no-oddities-here.js
@@ -1,8 +1,8 @@
 function noOdds( values ){
   let arr = [];
   for(let i=0; i<=values.length; i++){
-    if(values[i] % 2 === 0){
-      arr.push(values[i]);
+    if(values[i] % 2 === 0){ // if vals are even...
+      arr.push(values[i]); // push them into arr
     }
   }
   return arr;
