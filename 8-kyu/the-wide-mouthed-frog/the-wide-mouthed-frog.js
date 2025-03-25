ac3990bbc412285e1dfba514cd5ee61df8d83f74
Zahra Ali <zahraali.tech@gmail.com>
2025-03-25 00:18:32 -0400

    Update the-wide-mouthed-frog.js

--- 8-kyu/the-wide-mouthed-frog/the-wide-mouthed-frog.js
+++ 8-kyu/the-wide-mouthed-frog/the-wide-mouthed-frog.js
@@ -1,2 +1,8 @@
+// SOLUTION 1:
 const mouthSize = animal => /alligator/i.test(animal) ? 'small' : 'wide'; 
 // used regex + i to make the value case insensative
+
+// SOLUTION 2:
+function mouthSize(animal) {
+  return animal.toLowerCase() === "alligator" ? 'small' : 'wide';
+}
