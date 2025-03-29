518f3742aafd150ff60fed2c6ad82bcd0c31d201
Zahra Ali <zahraali.tech@gmail.com>
2025-03-29 03:05:03 -0400

    Update opposites-attract.js

--- 8-kyu/opposites-attract/opposites-attract.js
+++ 8-kyu/opposites-attract/opposites-attract.js
@@ -6,3 +6,8 @@ function lovefunc(flower1, flower2){
     return false;
   }
 }
+
+// alternative simple solution:
+function lovefunc(flower1, flower2){
+  return flower1 % 2 !== flower2 % 2; // one is even, other is odd
+}
