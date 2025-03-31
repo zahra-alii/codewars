6ee8df000b770c376d48c0227b3f6c6724effce2
Zahra Ali <zahraali.tech@gmail.com>
2025-03-31 18:09:56 -0400

    Update pythagorean-triple.js

--- 8-kyu/pythagorean-triple/pythagorean-triple.js
+++ 8-kyu/pythagorean-triple/pythagorean-triple.js
@@ -1,9 +1,19 @@
 // SOLUTION:
 function isPythagoreanTriple(integers) {
-  const [a,b,c] = integers.sort((a,b) => a < b ? -1 : 1); // turn 3 integers into variables, return it sorted
+  const [a,b,c] = integers.sort((a,b) => a < b ? -1 : 1); // deconstructure array & turn 3 integers into variables, return it sorted
   if (Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2)){ // if a^2 + b^2 = c^2...
     return true;
   }else{
     return false;
   }
 }
+
+// Alternative:
+function isPythagoreanTriple(integers) {
+  const [a,b,c] = integers.sort((a,b) => a-b);
+  if (a**2 + b**2 === c**2){
+    return true;
+  }else{
+    return false;
+  }
+} 
