903a209ed72a6aa336d4eb66463111ee024e98fb
Zahra Ali <zahraali.tech@gmail.com>
2025-03-30 21:35:46 -0400

    Update name-shuffler.js

--- 8-kyu/name-shuffler/name-shuffler.js
+++ 8-kyu/name-shuffler/name-shuffler.js
@@ -3,3 +3,8 @@ function nameShuffler(str){
   let [first, last] = str.split(' ');
   return last + ' ' + first;
 }
+
+// ALTERNATIVE:
+function nameShuffler(str){
+  return str.split(' ').reverse().join(' ')
+}
