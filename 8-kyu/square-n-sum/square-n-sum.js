38ddce5a593ac54cbac0805e11d2301da2d77a14
Zahra Ali <zahraali.tech@gmail.com>
2025-04-02 11:05:16 -0400

    Update solution - CodeHub

--- 8-kyu/square-n-sum/square-n-sum.js
+++ 8-kyu/square-n-sum/square-n-sum.js
@@ -1,19 +1,3 @@
-// Solution 1: .reduce() method
 function squareSum(numbers){
-  return numbers.reduce((acc, c) => acc + c**2, 0);
+  return numbers.reduce((total, c) => total + c**2, 0);
 }
-
-// Solution 2:
-function squareSum(numbers){
-  let total = 0;
-  for(let i=0; i<numbers.length; i++){
-    total += numbers[i]**2;
-  }
-  return total;
-}
-
-/* SOLUTION EXPLAINED: 
-- Declare total = 0
-- Create for loop
-- add total to every number squared (or num * num)
-- return total  */
