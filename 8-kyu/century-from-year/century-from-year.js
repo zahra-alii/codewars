d5f4adb33d3adeb2afe12db0f682d4da89369a46
Zahra Ali <zahraali.tech@gmail.com>
2025-04-04 13:14:45 -0400

    Add solution - CodeHub

---
+++ 8-kyu/century-from-year/century-from-year.js
@@ -0,0 +1,12 @@
+// SOLUTION:
+function century(year) {
+  if (year.toString().length < 3){
+    return 1;
+  }
+  const lastTwoDigits = +year.toString().slice(0,-2);
+  if(year % 100 === 0){
+    return lastTwoDigits;
+  }else{
+  return lastTwoDigits + 1;
+    }
+}
