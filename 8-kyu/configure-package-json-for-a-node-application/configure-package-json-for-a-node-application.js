4f74544acf9046d2b23e8db2423594b549b88bea
Zahra Ali <zahraali.tech@gmail.com>
2025-04-04 13:20:29 -0400

    Add solution - CodeHub

---
+++ 8-kyu/configure-package-json-for-a-node-application/configure-package-json-for-a-node-application.js
@@ -0,0 +1,8 @@
+// SOLUTION:
+const configuration = {
+  "name": "your-pack-name",
+  "description": "",
+  "version": "1.0.0"
+};
+// Hint: try creating with `npm init` command
+// Help! -> https://docs.npmjs.com/files/package.json
