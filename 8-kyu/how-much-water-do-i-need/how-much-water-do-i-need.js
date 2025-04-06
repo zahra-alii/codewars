adadf2d051b5e16f83013b9b4114d8b257ad0c8f
Zahra Ali <zahraali.tech@gmail.com>
2025-04-06 12:32:51 -0400

    Update how-much-water-do-i-need.js

--- 8-kyu/how-much-water-do-i-need/how-much-water-do-i-need.js
+++ 8-kyu/how-much-water-do-i-need/how-much-water-do-i-need.js
@@ -8,3 +8,14 @@ function howMuchWater(water, load, clothes){
       return Math.round(water * Math.pow(1.1, (clothes - load))*100) / 100; // code to run if clothes & water ratio match  
   }
 }
+
+// Alternative with .toFixed()
+function howMuchWater(water, load, clothes){
+  if (clothes > load*2){
+    return 'Too much clothes';
+  }else if(clothes < load){
+    return 'Not enough clothes'
+  }else{
+  return Number((water * Math.pow(1.1, clothes - load)).toFixed(2)); // code to run if clothes & water ratio match, return solution to 2 decimal places
+  }
+}
