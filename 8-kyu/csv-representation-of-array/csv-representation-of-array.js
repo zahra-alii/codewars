150a5f0d7d3b02477ca70c5f574a5305a97d7900
Zahra Ali <zahraali.tech@gmail.com>
2025-04-07 13:08:35 -0400

    Update csv-representation-of-array.js

--- 8-kyu/csv-representation-of-array/csv-representation-of-array.js
+++ 8-kyu/csv-representation-of-array/csv-representation-of-array.js
@@ -2,5 +2,8 @@
 function toCsvText(array) {
   const withCommas = array.map(n => n.join(',')); // removes spaces between every elem
   return withCommas.join('\n'); // with the new elements, add '\n' to output
-
 }
+
+
+// Alternative:
+const toCsvText = array => array.join('\n');
