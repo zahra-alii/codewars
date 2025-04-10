a057dbd7a12e415c7f3f3d86038581ccef0ea6f8
Zahra Ali <zahraali.tech@gmail.com>
2025-04-10 17:17:44 -0400

    Update correct-the-mistakes-of-the-character-recognition-software.js

--- 8-kyu/correct-the-mistakes-of-the-character-recognition-software/correct-the-mistakes-of-the-character-recognition-software.js
+++ 8-kyu/correct-the-mistakes-of-the-character-recognition-software/correct-the-mistakes-of-the-character-recognition-software.js
@@ -1,19 +1,19 @@
 // SOLUTION:
 function correct(str){
-  let newStr = ''; // decl empty str
-  for(const char of str){
-    if (char === '5'){
-      newStr += 'S';
-    }else if(char === '0'){
-      newStr += 'O';
-    }else if(char === '1'){
-      newStr += 'I';
-    }else{
-      newStr += char;
+    let newStr = ''; // decl empty str
+    for(const char of str){
+      if (char === '5'){
+        newStr += 'S';
+      }else if(char === '0'){
+        newStr += 'O';
+      }else if(char === '1'){
+        newStr += 'I';
+      }else{
+        newStr += char;
+      }
     }
+    return newStr;
   }
-  return newStr;
-}
-
-// Alternative using regex: 
-correct = s => s.replace(/0/g,'O').replace(/1/g,'I').replace(/5/g,'S')
+  
+  // Alternative using regex: 
+  const correct = s => s.replace(/0/g,'O').replace(/1/g,'I').replace(/5/g,'S')
